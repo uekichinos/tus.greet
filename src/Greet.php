9cb5d2943f895cd209994ea5e71a46dd536d48f7
Megat Zulkhairi Megat Mukhtar <megatzulkhairi@gmail.com>
2021-10-10 19:03:49 +0800

    update

--- src/Greet.php
+++ src/Greet.php
@@ -2,5 +2,25 @@
 
 namespace TheUsual\Greet;
 
-class Greet{
+class Greet
+{
+
+    public static function hello($name = null)
+    {
+        $time = date("H");
+
+        if ($time < "12") {
+            echo "Good morning";
+        } else if ($time >= "12" && $time < "17") {
+            echo "Good afternoon";
+        } else if ($time >= "17" && $time < "19") {
+            echo "Good evening";
+        } else if ($time >= "19") {
+            echo "Good night";
+        }
+
+        if ($name != null) {
+            echo ", " . $name;
+        }
+    }
 }
